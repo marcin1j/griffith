1dfd413ebfc3a4b80ce6e86a91e214fa11842a19
mikej06 <>
2013-06-26 20:59:58 +0000

    [1194709] DVDEmpire plugin stopped returning results

--- lib/plugins/movie/PluginMovieDVDEmpire.py
+++ lib/plugins/movie/PluginMovieDVDEmpire.py
@@ -24,6 +24,7 @@ __revision__ = '$Id$'
 import gutils
 import movie
 import re
+import string
 
 plugin_name         = "DVD Empire"
 plugin_description  = "International Retailer of DVD Movies"
@@ -31,57 +32,68 @@ plugin_url          = "www.dvdempire.com"
 plugin_language     = _("English")
 plugin_author       = "Michael Jahn"
 plugin_author_email = "<mikej06@hotmail.com>"
-plugin_version      = "1.1"
+plugin_version      = "1.2"
 
 class Plugin(movie.Movie):
     def __init__(self, id):
         self.encode   = 'iso-8859-1'
         self.movie_id = id
-        self.url      = "http://www.dvdempire.com/Exec/v4_item.asp?item_id=" + str(self.movie_id)
+        self.url      = "http://www.dvdempire.com" + str(self.movie_id)
 
     def get_image(self):
-        tmp_page = gutils.trim(self.page, '<td align="center" valign="top" class="fontsmall">', '</td>')
-        self.image_url = gutils.trim(tmp_page, '<img src=\'', '\' ')
+        tmp_page = gutils.trim(self.page, 'id="Boxcover"', '</div>')
+        self.image_url = gutils.trim(tmp_page, 'href="', '"')
 
     def get_o_title(self):
-        self.o_title = gutils.strip_tags(gutils.trim(self.page,'<td class="fontxlarge" valign="top" align="left">', '<nobr>'))
+        self.o_title = gutils.strip_tags(gutils.trim(self.page, '<title>', '('))
 
     def get_title(self):
-        self.title = gutils.strip_tags(gutils.trim(self.page,'<td class="fontxlarge" valign="top" align="left">', '<nobr>'))
+        self.title = gutils.strip_tags(gutils.trim(self.page, '<title>', '('))
 
     def get_director(self):
-        self.director = gutils.strip_tags(gutils.trim(self.page,">Directors:","</a>"))
+        self.director = gutils.trim(self.page, '<h3>Director</h3>', '</div>')
+        self.director = self.director.replace('</li><li', '</li>, <li')
         self.director = self.director.replace('&nbsp;', '')
         self.director = self.director.replace('&#149;', '')
 
     def get_plot(self):
-        self.plot = gutils.strip_tags(gutils.trim(self.page, '<td width="100%" valign="top" class="fontsmall3">', '</td>'))
+        self.plot = gutils.strip_tags(gutils.trim(self.page, 'Synopsis</h2>', '</div>'))
         self.plot = self.plot.replace(u'\x93', '"')
         self.plot = self.plot.replace(u'\x84', '"')
 
     def get_year(self):
-        self.year = gutils.strip_tags(gutils.trim(self.page, '>Production Year:', '<br />'))
+        self.year = gutils.strip_tags(gutils.after(gutils.trim(self.page, 'Production Year', '<br'), '>'))
 
     def get_runtime(self):
-        self.runtime = gutils.strip_tags(gutils.trim(self.page, '>Length:', '<br />'))
+        self.runtime = ''
+        tmp = gutils.strip_tags(gutils.trim(self.page, 'Length', '<br'))
+        #1 hrs. 59 mins.
+        try:
+            self.runtime = int(gutils.before(tmp, 'hrs')) * 60 + int(gutils.trim(tmp, '.', 'mins'))
+        except:
+            self.runtime = ''
 
     def get_genre(self):
-        self.genre = gutils.strip_tags(gutils.trim(self.page, '>Genre</b>:', '</tr>'))
+        tmp = gutils.strip_tags(gutils.trim(self.page, '<h2>Categories</h2>', '</p>'))
+        tmp = tmp.replace('\r\n', '')
+        self.genre = re.sub('[ \t]+', ' ', tmp)
 
     def get_cast(self):
-        self.cast = gutils.trim(self.page, '>Actors:', '</td><td')
+        self.cast = gutils.trim(self.page, '<h3>Cast</h3>', '</div>')
         self.cast = self.cast.replace('<br>', '\n')
         self.cast = self.cast.replace('<br />', '\n')
         self.cast = self.cast.replace('&nbsp;', '')
         self.cast = self.cast.replace('&#8226;', '')
         self.cast = self.cast.replace('&#149;', '')
+        self.cast = self.cast.replace('</li><li', '</li>\n<li')
+        self.cast = self.cast.replace('</li><!-- non-headliners --><li', '</li>\n<li')
         self.cast = gutils.strip_tags(self.cast)
 
     def get_classification(self):
-        self.classification = gutils.strip_tags(gutils.trim(self.page, '>Rating:', '<br />'))
+        self.classification = gutils.strip_tags(gutils.trim(self.page, 'Rating</strong>', '<br'))
 
     def get_studio(self):
-        self.studio = gutils.strip_tags(gutils.trim(self.page, '>Studio:', '<br />'))
+        self.studio = gutils.strip_tags(gutils.trim(self.page, 'Studio</strong>', '<br'))
 
     def get_o_site(self):
         self.o_site = ""
@@ -96,7 +108,7 @@ class Plugin(movie.Movie):
         self.country = ""
 
     def get_rating(self):
-        self.rating = gutils.clean(gutils.trim(self.page, '>Overall Rating:', ' out of'))
+        self.rating = gutils.clean(gutils.trim(self.page, 'Overall Rating:', ' out of'))
         try:
             tmp_float = float(self.rating)
             tmp_float = round(2 * tmp_float, 0)
@@ -106,90 +118,65 @@ class Plugin(movie.Movie):
 
     def get_notes(self):
         self.notes = ''
-        tmp_page = gutils.trim(self.page, 'Features:', '<b>')
+        tmp_page = gutils.trim(self.page, '<h3>Features</h3>', '</p>')
         tmp_page = tmp_page.replace('<br>', '\n')
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
-            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
-            self.notes = self.notes + '\nFeatures:' + tmp_page + '\n'
-        tmp_page = gutils.trim(self.page, 'Video:', '<b>')
+            self.notes = self.notes + 'Features:\n' + tmp_page + '\n\n'
+        tmp_page = gutils.trim(self.page, 'Video</strong>', '<strong>')
+        tmp_page = tmp_page.replace('\r\n', '')
+        tmp_page = re.sub('[ \t]+', ' ', tmp_page)
         tmp_page = tmp_page.replace('<br>', '\n')
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
-            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
-            self.notes = self.notes + '\nVideo:' + tmp_page + '\n'
-        tmp_page = gutils.trim(self.page, 'Audio:', '<b>')
+            self.notes = self.notes + 'Video:' + tmp_page
+        tmp_page = gutils.trim(self.page, 'Audio</strong>', '</div>')
+        tmp_page = tmp_page.replace('\r\n', '')
+        tmp_page = re.sub('[ \t]+', ' ', tmp_page)
         tmp_page = tmp_page.replace('<br>', '\n')
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = tmp_page.replace('(more info)', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
-            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
-            self.notes = self.notes + '\nAudio:' + tmp_page
-        tmp_page = gutils.trim(self.page, 'Subtitles:', '<b>')
-        tmp_page = gutils.strip_tags(tmp_page)
-        if tmp_page <> '':
-            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
-            self.notes = self.notes + '\nSubtitles:\n' + tmp_page + '\n'
+            self.notes = self.notes + 'Audio:' + tmp_page
 
     def get_screenplay(self):
-        self.screenplay = gutils.strip_tags(gutils.trim(self.page, '>Writers:', '</a>'))
+        self.screenplay = gutils.strip_tags(gutils.trim(self.page, 'Writers</strong>', '<br'))
         self.screenplay = self.screenplay.replace('&nbsp;', '')
         self.screenplay = self.screenplay.replace('&#149;', '')
 
     def get_barcode(self):
-        self.barcode = gutils.trim(self.page, '<b>UPC Code:</b>', '<')
+        self.barcode = gutils.trim(self.page, 'UPC Code</strong>', '<br')
+
 
 class SearchPlugin(movie.SearchMovie):
     def __init__(self):
-        self.original_url_search   = "http://www.dvdempire.com/Exec/v1_search_all.asp?&site_media_id=0&pp=&search_refined=32&used=0&string="
-        self.translated_url_search = "http://www.dvdempire.com/Exec/v1_search_all.asp?&site_media_id=0&pp=&search_refined=32&used=0&string="
+        self.original_url_search   = "http://www.dvdempire.com/allsearch/search?pageSize=100&q="
+        self.translated_url_search = "http://www.dvdempire.com/allsearch/search?pageSize=100&q="
         self.encode                = 'iso-8859-1'
 
     def search(self,parent_window):
         if not self.open_search(parent_window):
             return None
-        # short the content
-        tmp_page = gutils.trim(self.page,'<select name="sort"', 'Click Here to make a Suggestion</a>')
-        #
-        # try to get all result pages (not so nice, but it works)
-        #
-        tmp_pagecount = gutils.trim(self.page, '<div id="Search_Container" name="Search_Container">', '</table>')
-        tmp_pagecountintuse = 1
-        elements = tmp_pagecount.split("&page=")
-        for element in elements:
-            try:
-                tmp_pagecountint = int(gutils.before(element, '\''))
-            except:
-                tmp_pagecountint = 0
-            if tmp_pagecountint > tmp_pagecountintuse:
-                tmp_pagecountintuse = tmp_pagecountint
-        tmp_pagecountintcurrent = 1
-        while tmp_pagecountintuse > tmp_pagecountintcurrent and tmp_pagecountintuse < 4:
-            tmp_pagecountintcurrent = tmp_pagecountintcurrent + 1
-            self.url = "http://www.dvdempire.com/Exec/v1_search_all.asp?&site_media_id=0&pp=&search_refined=32&used=0&page=" + str(tmp_pagecountintcurrent) + "&string="
-            self.open_search(parent_window)
-            if self.open_search(parent_window):
-                tmp_page2 = gutils.trim(self.page,'<select name="sort"', 'Click Here to make a Suggestion</a>')
-                tmp_page = tmp_page + tmp_page2
-
-        self.page = tmp_page
         return self.page
 
     def get_searches(self):
-        split_pattern = re.compile('<a[\t ]+href=["\']/Exec/v4_item[.]asp[?](?:userid=[-0-9]+[&]amp;)*item_id=([0-9]+)\'')
+        split_pattern = re.compile('class="title"')
         elements = split_pattern.split(self.page)
-        for index in range(1, len(elements), 2):
-            title_element = elements[index + 1]
-            tmp_title = gutils.strip_tags(gutils.trim(title_element, '>', '</a>'))
+        for index in range(1, len(elements), 1):
+            title_element = elements[index]
+            tmp_title = gutils.clean(gutils.strip_tags(gutils.trim(title_element, '>', '</a>')))
+            tmp_title = string.replace(tmp_title, '&nbsp;', ' ')
+            tmp_title = string.replace(tmp_title, '&amp;', '&')
+            tmp_title = string.replace(tmp_title, '&quot;', '"')
             if tmp_title <> '':
-                self.ids.append(elements[index])
-                type = re.search('<b>Blu-ray</b>', title_element)
+                self.ids.append(gutils.trim(elements[index], 'href="', '"'))
+                type = re.search('>Blu-ray</span>', title_element)
                 if type:
                     tmp_title = tmp_title + ' (Blu-ray)'
-                type = re.search('<b>DVD-Video</b>', title_element)
+                type = re.search('>DVD-Video</span>', title_element)
                 if type:
                     tmp_title = tmp_title + ' (DVD)'
                 self.titles.append(tmp_title)
@@ -203,7 +190,7 @@ class SearchPluginTest(SearchPlugin):
     # dict { movie_id -> [ expected result count for original url, expected result count for translated url ] }
     #
     test_configuration = {
-        'Rocky Balboa' : [ 2, 2 ],
+        'Rocky Balboa' : [ 30, 30 ],
     }
 
 class PluginTest:
@@ -215,45 +202,45 @@ class PluginTest:
     #        * or the expected value
     #
     test_configuration = {
-        '1300280' : { 
-            'title'               : 'Rocky Balboa',
-            'o_title'             : 'Rocky Balboa',
-            'director'            : 'Sylvester Stallone',
+        '/1402424/meet-the-spartans-unrated-pit-of-death-edition-blu-ray.html' : { 
+            'title'               : 'Meet The Spartans: Unrated Pit Of Death Edition',
+            'o_title'             : 'Meet The Spartans: Unrated Pit Of Death Edition',
+            'director'            : 'Jason Friedberg, Aaron Seltzer',
             'plot'                : True,
-            'cast'                : 'Sylvester Stallone\n\
-Burt Young\n\
-Geraldine Hughes\n\
-Milo Ventimiglia\n\
-Antonio Tarver',
+            'cast'                : 'Carmen Electra\n\
+Sean Maguire\n\
+Ken Davitian',
             'country'             : False,
-            'genre'               : 'Sports, Boxing, Drama',
-            'classification'      : 'PG',
-            'studio'              : 'Sony Pictures Home Entertainment',
+            'genre'               : 'Blu-ray, Comedy, Movies, Spoofs & Parodies',
+            'classification'      : 'NR',
+            'studio'              : '20th Century Fox',
             'o_site'              : False,
-            'site'                : 'http://www.dvdempire.com/Exec/v4_item.asp?item_id=1300280',
+            'site'                : 'http://www.dvdempire.com/1402424/meet-the-spartans-unrated-pit-of-death-edition-blu-ray.html',
             'trailer'             : False,
-            'year'                : 2006,
+            'year'                : 2008,
             'notes'               : 'Features:\n\
-Deleted Scenes & Alternate Ending\n\
-Boxing\'s Bloopers\n\
-Skill Vs. Will: The Making of Rocky Balboa\n\
-Reality in the Ring: Filming Rocky\'s Final Fight\n\
-Virtual Champion: Creating the Computer Fight\n\
-Audio Commentary with Sylvester Stallone\n\
+Audio Commentary By Cast And Crew\n\
+Prepare For Thrusting Featurette\n\
+Tour The Set With Ike Barinholtz Featurette\n\
+Gag Reel\n\
+Know Your Spartans Pop Culture Trivia Game\n\
+Celebrity Kickoff Game\n\
+Super Pit-Of-Death Ultimate Tactical Battle Challange Game\n\
+Meet The Spartans: The Music\n\
+Trivial Track\n\
+Theatrical Trailers\n\
 \n\
 Video:\n\
-Widescreen 1.85:1 Color (Anamorphic) \n\
+ Widescreen 1.85:1 Color \n\
 \n\
-Audio: \n\
-ENGLISH: Dolby Digital 5.1 [CC]\n\
-FRENCH: Dolby Digital 5.1\n\
-\n\
-Subtitles:\n\
-English, Spanish, French',
-            'runtime'             : 102,
+Audio:\n\
+ French Dolby Digital Surround \n\
+ Spanish Dolby Digital Surround \n\
+ English DTS-HD MA 5.1',
+            'runtime'             : 87,
             'image'               : True,
-            'rating'              : 9,
-            'screenplay'          : 'Sylvester Stallone',
-            'barcode'             : '043396161900'
+            'rating'              : 5,
+            'screenplay'          : False,
+            'barcode'             : '024543522614'
         },
     }
